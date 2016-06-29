424e53aff2c6497bfecb50358a1fefeddac06d59
lsh <812937199@qq.com>
2016-06-29 10:25:23 +0800

    change  readme

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # ReplaceWebUrl
-First of all, a few screenshots:
+First of all, a screenshot:
 
-![screenshot1]( https://github.com/fossilhua/ReplaceWebUrl/blob/master/gif/screenshot_1.gif )
+![screenshot1]( https://github.com/fossilhua/ReplaceWebUrl/blob/master/gif/gif1.gif )
 
-## Demo
+[Demo APK下载]()
+
+## How to Use
+
+###### 1.Integration jcenter
+
+Please follow the latest publishing aar on jcenter.
+
+follow by the gradle code
+```gradle
+dependencies{
+    compile 'com.lsh:ReplaceWebUrl:1.0.0'
+}
+```
+
+###### 2.Usage:
+
+``` 
+LinkUtil linkUtil = new LinkUtil(this);
+linkUtil.setReplaceStr("\uE620网络链接");
+linkUtil.applyUrl(textView, des);
+linkUtil.setOnClickUrl(new LinkUtil.OnClickUrl() {
+    @Override
+    public void onClick(View widget, String url) {
+        Toast.makeText(MainActivity.this, "url:" + url, Toast.LENGTH_SHORT).show();
+    }
+});
+```
+### Version Log
+* ***v1.0.0*** ...
+
+
--- app/src/main/java/com/lsh/spandemo/MainActivity.java
+++ app/src/main/java/com/lsh/spandemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.lsh.spandemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -9,23 +10,32 @@ import android.widget.Toast;
 import com.lsh.changeweburllib.LinkUtil;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
-    String str = "\uD83D\uDE03测试http://baidu.com 测试http://ddddd.com \uD83D\uDE03";
+
+    private TextView mTvBefore;
+    private TextView mTvAfter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) findViewById(R.id.tv_des);
-        LinkUtil linkUtil=new LinkUtil(this);
+        mTvBefore = (TextView) findViewById(R.id.tv_before);
+        mTvAfter = (TextView) findViewById(R.id.tv_after);
+        mTvBefore.setText(getString(R.string.text_test_str));
+    }
+
+    public void ChangeText(View v) {
+        String des = mTvBefore.getText().toString().trim();
+        if (TextUtils.isEmpty(des)) {
+            return;
+        }
+        LinkUtil linkUtil = new LinkUtil(this);
         linkUtil.setReplaceStr("\uE620网络链接");
-        linkUtil.applyUrl(tv,str);
+        linkUtil.applyUrl(mTvAfter, des);
         linkUtil.setOnClickUrl(new LinkUtil.OnClickUrl() {
             @Override
             public void onClick(View widget, String url) {
-                Toast.makeText(MainActivity.this,"url:"+url,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "url:" + url, Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 }
